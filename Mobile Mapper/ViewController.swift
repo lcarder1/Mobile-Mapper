9316281b36ed99dcf59293cc58ab5f2a6d16df29
Lauren Carder <lcarder@laurens-macbook-air-9.local>
2019-03-08 11:37:13 -0600

    Added a location to the map and added user location

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    let barringtonAnnotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let latitude = 42.15704
+        let longitude = -88.14812
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        barringtonAnnotation.coordinate = coordinate
+        barringtonAnnotation.title = "Barrington High School"
+        mapView.addAnnotation(barringtonAnnotation)
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
 
 
